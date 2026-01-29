56e873a642645664db6e25a4fe13d0db650fb72a
samuelfrr2005 <samuelfrr2005@gmail.com>
2026-01-29 16:47:35 +0100

    Update db_credentials.php

--- php/db_credentials.php
+++ php/db_credentials.php
@@ -1,6 +1,6 @@
 <?php
     define('DB_HOST', 'localhost');
     define('DB_USER', 'almsa018sql3');
-    define('DB_PW', 'wtxuniUepT');
+    define('DB_PW', '');
     define('DB_NAME', 'almsa018sql3');
 ?>
